4601f9a82f4ac11ea103fb99704f18a21e19527c
chenshasha <chenshasha@zhiyitech.cn>
2020-02-16 22:11:28 +0800

    add

---
+++ src/components/navbar/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="nav-bar">
+      <div class="left"><slot name="left"/></div>
+      <div class="center"><slot name="center"/></div>
+      <div class="right"><slot name="right"/></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.nav-bar{
+    display: flex;
+    line-height: 44px;
+    height: 44px;
+}
+.left,.right{
+    width: 60px;
+}
+.center{
+    flex: 1;
+    text-align: center;
+    color: #fff;
+    font-size: 18px;
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
+    <nav-bar class="home-nav">
+      <div slot="center">测试一下</div>
+    </nav-bar>
       <h1>首页</h1>
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/navbar/NavBar'
+import {getHomeData} from '@/network/home'
 export default {
-
+  components:{NavBar},
+  created(){
+    getHomeData().then(res=>{
+      console.log(res);
+      
+    })
+  }
 }
 </script>
 
 <style>
-
+.home-nav{
+    background: lightcoral;
+}
 </style>
