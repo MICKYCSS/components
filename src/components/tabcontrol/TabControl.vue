182067ea583738164b560ff2a46c9600af140d8a
chenshasha <chenshasha@zhiyitech.cn>
2020-02-18 10:11:08 +0800

    add

---
+++ src/components/tabcontrol/TabControl.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="tab-control">
+      <div :key="index" v-for="(item,index) in titles" 
+      class="tab-control-item"
+      :class="{'active':index===currentIndex}" @click="itemClick(index)">
+          <span>{{item}}</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        titles:{
+            type:Array,
+            default:()=>[]
+        }
+    },
+    data(){
+        return{
+            currentIndex:0
+        }
+    },
+    methods:{
+        itemClick(index){
+            this.currentIndex = index
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.tab-control{
+    display:flex;
+     text-align: center;
+    height: 44px;
+    line-height: 44px;
+    background: #fff;
+}
+.tab-control-item{
+    flex: 1; 
+}
+.tab-control-item span{
+    padding: 5px;
+}
+.active span{
+    color: lightcoral;
+    border-bottom: 3px solid lightcoral;
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,17 +1,26 @@
 <template>
-  <div>
+  <div id="home">
     <nav-bar class="home-nav">
       <div slot="center">测试一下</div>
     </nav-bar>
       <h1>首页</h1>
+      <tab-control class="tab-control" :titles="titles"></tab-control>
+      <li v-for="(item,index) in list" :key="index">{{item}}</li>
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/navbar/NavBar'
+import TabControl from '@/components/tabcontrol/TabControl'
 import {getHomeData} from '@/network/home'
 export default {
-  components:{NavBar},
+  components:{NavBar,TabControl},
+  data(){
+    return{
+      titles:["流行","新款","精选"],
+      list:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18]
+    }
+  },
   created(){
     getHomeData().then(res=>{
       console.log(res);
@@ -22,7 +31,19 @@ export default {
 </script>
 
 <style>
+#home{
+  padding: 44px 0;
+}
 .home-nav{
     background: lightcoral;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+}
+.tab-control{
+  position: sticky;
+  top: 44px;
+
 }
 </style>
