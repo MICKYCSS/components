853b73bea3289c9b374c30d8a10d9f7133e684c8
chenshasha <chenshasha@zhiyitech.cn>
2020-03-30 15:34:00 +0800

    add questions

---
+++ src/components/Test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+     <div :class="[isActive?activeClass:'',errorClass]">{{value}}</div> 
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        value:String
+    },
+    data(){
+        return{
+            isActive:true,
+            activeClass:'active',
+            errorClass:'error'
+        }
+    },
+    methods:{
+       
+    }
+}
+</script>
+
+<style>
+.active{
+    color: red;
+}
+.error{
+    background: black;
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,29 +3,53 @@
     <nav-bar class="home-nav">
       <div slot="center">测试一下</div>
     </nav-bar>
-      <h1>首页</h1>
+    <Test :ref="'test'" v-model="value"/>
+    <ul>
+      <li :key="item" v-for="item in list">{{item}}</li>
+    </ul>
+    <button @click="onClick">change list</button>
+    <input type="text" :value="value" @input="onInput">{{value}}
+      <!-- <h1>首页</h1>
       <tab-control class="tab-control" :titles="titles"></tab-control>
-      <li v-for="(item,index) in list" :key="index">{{item}}</li>
+      <li v-for="(item,index) in list" :key="index">{{item}}</li> -->
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/navbar/NavBar'
+import Test from '@/components/Test'
 import TabControl from '@/components/tabcontrol/TabControl'
 import {getHomeData} from '@/network/home'
 export default {
-  components:{NavBar,TabControl},
+  components:{NavBar,TabControl,Test},
   data(){
     return{
       titles:["流行","新款","精选"],
-      list:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18]
+      list:[1,2,3,4,5],
+      value:'value value'
     }
   },
   created(){
-    getHomeData().then(res=>{
-      console.log(res);
+    // getHomeData().then(res=>{
+    //   console.log(res);
       
-    })
+    // })
+    
+  },
+  mounted(){
+    console.log(this.$refs.test);
+
+  },
+  methods:{
+    onClick(){
+      console.log('onClick');
+      
+      this.list[0]=100
+      this.$set(this.list,0,100)
+    },
+    onInput(e){
+      this.value = e.target.value
+    }
   }
 }
 </script>
